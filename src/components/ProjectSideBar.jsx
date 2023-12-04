ee5dd46831ef2f78218f399a888efe078a519620
Sreerag Mohan <sreeraagmohancv@gmail.com>
2023-12-04 14:03:34 +0530

    mapped the title on sidebar

--- src/components/ProjectSideBar.jsx
+++ src/components/ProjectSideBar.jsx
@@ -2,20 +2,23 @@ import React from "react";
 import NewProject from "./NewProject";
 import Button from "./Button";
 
-function ProjectSideBar({onStartAddProject}) {
+function ProjectSideBar({ onStartAddProject, projects }) {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
         Your Projects
       </h2>
       <div>
-        <Button onClick={onStartAddProject}>
-        + Add Project
-        </Button>
-        
+        <Button onClick={onStartAddProject}>+ Add Project</Button>
       </div>
-      <ul>
-
+      <ul className="mt-8">
+        {projects.map((project) => {
+          return <li key={project.id}>
+            <button className="w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800">
+            {project.title}
+            </button> 
+            </li>;
+        })}
       </ul>
     </aside>
   );
