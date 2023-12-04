d5185391e2f946b8d0230da02b96475de3b806f7
Sreerag Mohan <sreeraagmohancv@gmail.com>
2023-12-04 11:11:29 +0530

    hooks added (useState)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
 import NewProject from "./components/NewProject";
 import ProjectSideBar from "./components/ProjectSideBar";
+import NoProjectSelected from "./components/NoProjectSelected";
+import { useState } from "react";
 
 function App() {
+
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  })
+
+  function handleStartAddProject(){
+    setProjectsState((prevProjectsState) => {
+      return {
+        ...prevProjectsState,
+        selectedProjectId: null
+      }
+    })
+  }
+
+  let content;
+
+  if(projectsState.selectedProjectId === null){
+    content = <NewProject/>
+  } else if(projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+  }
+
+
   return (
     <>
     <main className="h-screen my-8 flex gap-8">
-    <ProjectSideBar/>
-    <NewProject/>
+    <ProjectSideBar onStartAddProject={handleStartAddProject} />
+    {content}
     </main>
     </>
   );
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -3,16 +3,20 @@ import Input from "./Input";
 
 function NewProject() {
   return (
-    <div>
-        <menu className="flex items-center justify-end gap-4 my-4s">
-            <button>CANCEL</button>
-            <button>SAVE</button>
-        </menu>
-        <div>
-            <Input label='Title' />
-            <Input label='Description' textarea />
-            <Input label='Due Date' />
-        </div>
+    <div className="w-[35rem] mt-16">
+      <menu className="flex items-center justify-end gap-4 my-4s">
+        <li>
+          <button className="text-stone-800 hover:text-stone-950">CANCEL</button>
+        </li>
+        <li>
+          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">SAVE</button>
+        </li>
+      </menu>
+      <div>
+        <Input label="Title" />
+        <Input label="Description" textarea />
+        <Input label="Due Date" />
+      </div>
     </div>
   );
 }
